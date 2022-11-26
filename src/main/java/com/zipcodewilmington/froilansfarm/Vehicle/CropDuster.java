9a317b72ac55fe0f44a903dad526ada812ea10a2
Tiffany678 <52tiffany.yang@gmail.comgit config --global user.name Tiffany678git config --global user.email 52tiffany.yang@gmail.com>
2022-11-26 15:29:36 -0500

    add methods to the Vehicle super Class and its subclass

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/CropDuster.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/CropDuster.java
@@ -1,14 +1,20 @@
 package com.zipcodewilmington.froilansfarm.Vehicle;
 
-import com.zipcodewilmington.froilansfarm.Interfaces.Ride.Aircraft;
+import com.zipcodewilmington.froilansfarm.StorageClasses.Farm.Farm;
+import com.zipcodewilmington.froilansfarm.StorageClasses.Field.CropRow;
 
-public class CropDuster extends FarmVehicle implements Aircraft {
+public class CropDuster extends Aircraft {
 
-    public boolean mounted() {
-        return false;
+    //public boolean mounted() {
+//        return false;
+//    }
+
+
+    public void fertilize(CropRow cropRow1){
+
     }
 
-    public void fly() {
+    public void operate(Farm farm1){
 
     }
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/FarmVehicle.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicle/FarmVehicle.java
@@ -1,7 +1,18 @@
 package com.zipcodewilmington.froilansfarm.Vehicle;
 
+import com.zipcodewilmington.froilansfarm.Interfaces.NoiseMaker;
 import com.zipcodewilmington.froilansfarm.Interfaces.Ride.Equipment;
+import com.zipcodewilmington.froilansfarm.Interfaces.Ride.Rideable;
+import com.zipcodewilmington.froilansfarm.StorageClasses.Farm.Farm;
 
-public class FarmVehicle {
+public abstract class FarmVehicle extends Vehicle implements NoiseMaker, Rideable {
+    public void makeNoise(){
 
+    }
+    public boolean mounted(){
+        return false;
+    }
+    public void operate(Farm farm1){
+
+    }
 }
